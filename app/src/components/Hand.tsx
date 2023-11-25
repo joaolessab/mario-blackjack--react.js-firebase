d47bd0e185bcfbfaac0dd50587e364ac3bd08be7
joaolessab <joaovitorlessa@gmail.com>
2023-11-25 18:01:10 -0300

    feat(app): Hand component

---
+++ app/src/components/Hand.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Card from "./Card";
+
+interface IHand {
+  hand: { value: string; suit: string }[];
+  type: string;
+  score: string | number;
+  isGameOver?: boolean;
+}
+
+const Hand: React.FC<IHand> = ({
+  hand,
+  type,
+  score,
+  isGameOver,
+}) => (
+  <div className="dealer-hand">
+    <h2>{type} Hand</h2>
+
+    <div className="hand">
+      {hand.map((card, index) => (
+        <React.Fragment key={index}>
+          {type === "player" ? (
+            <Card {...card} show />
+          ) : (
+            // Show the first card face up, and others face down
+            // Show all dealer's cards if the game is over
+            <Card {...card} show={index === 0 || isGameOver} />
+          )}
+        </React.Fragment>
+      ))}
+    </div>
+
+    <p>{score}</p>
+  </div>
+);
+
+export default Hand;
